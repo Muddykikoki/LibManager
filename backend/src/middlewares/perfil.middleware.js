4b49d30962081671d8100896ec4d7b4adf90eb44
marlos <marlos41952@fiec.edu.br>
2026-06-19 20:28:40 -0300

    refector: facilitando a HIERARQUIA

--- backend/src/middlewares/perfil.middleware.js
+++ backend/src/middlewares/perfil.middleware.js
@@ -1,14 +1,15 @@
 const HIERARQUIA = { LEITOR: 0, BIBLIOTECARIO: 1, DEV: 2 };
 
+function resolverNivel(p) {
+  if (typeof p === "number") return p;
+  return HIERARQUIA[p];
+}
+
 export function exigirPerfil(...perfisPermitidos) {
   return (req, res, next) => {
-    const nivelUser = HIERARQUIA[req.user?.nivel_perfil];
-    console.log("Perfil do usuário:", req.user?.nivel_perfil);
+    const nivelUser = resolverNivel(req.user?.nivel_perfil);
     const temPermissao = perfisPermitidos.some(
-      (p) => {
-        console.log(nivelUser, HIERARQUIA[p]);
-        return nivelUser >= HIERARQUIA[p];
-      }
+      (p) => nivelUser >= resolverNivel(p)
     );
     if (!temPermissao)
       return res.status(403).json({ message: "Acesso negado" });
